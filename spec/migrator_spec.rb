585c934df11da63722bbad7ec98f6352c6cd4a25
ryopeko <ryopeko@gmail.com>
2014-09-26 21:18:33 +0900

    Add DBSupplier::Migrator.migrate's spec

--- spec/migrator_spec.rb
+++ spec/migrator_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper.rb'
 require 'db_supplier/migrator'
+require 'active_record'
+require 'logger'
 
 def migrate_class
   DBSupplier::Migrator.dup
@@ -79,6 +81,45 @@ describe DBSupplier::Migrator do
       allow_any_instance_of(Octokit::Client).to receive(:contents).and_return('sql')
 
       it { expect(set_all_args.fetch_sql(db_name)).to eq ['sql'] }
+    end
+  end
+
+  describe ".migrate" do
+    context "when migrator can execute query" do
+      before do
+        @db_name = 'external_db_test.sqlite3'
+
+        ActiveRecord::Base.configurations = {
+          'external_database_test' => {
+            adapter: 'sqlite3',
+            database: @db_name
+          }
+        }
+
+        migrator = migrate_class.tap {|s|
+          s.configurations = {
+            schema_repository: 'repo_name',
+            access_token: 'access_token',
+            schema_files: { external_database_test: 'foo.sql' },
+            logger: Logger.new('/dev/null')
+          }
+        }
+        @table_name = 'foo_table'
+
+        allow(migrator.client).to receive(:contents).and_return(
+          "CREATE TABLE \"#{@table_name}\" (\"id\" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, \"name\" varchar(255) NOT NULL)"
+        )
+
+        migrator.migrate
+      end
+
+      after do
+        FileUtils.rm @db_name
+      end
+
+
+
+      it { expect(ActiveRecord::Base.connection.tables).to include(@table_name) }
     end
   end
 end
